02e65285c13bc9b7d186b2b3569aec9329db4628
Marcelo Frasca <mvfrasca@gmail.com>
2019-12-10 20:17:40 -0300

    Exercício 4.1

--- jogos/forca.py
+++ jogos/forca.py
@@ -1,3 +1,5 @@
+import random
+
 # Inicialização
 palavra_secreta = "banana"
 qtd_tentativas = 7
@@ -8,23 +10,42 @@ palavra_descoberta = []
 while len(palavra_descoberta) < len(palavra_secreta):
     palavra_descoberta.append("_")
 
+print("************************************************************")
+print("                     JOGO DA FORCA")
+print("************************************************************\n")
+
+# Loop do Jogo
 while (not ganhou and not enforcou):
     # Recupera o chute do usuário
     chute = input("Chute uma letra: ")
-    if chute in palavra_secreta:
+    # Verifica existência da letra na palavra
+    if chute in palavra_descoberta:
+        print("\nO fiiiii! Essa letra você já chutou!")
+    elif chute in palavra_secreta:
+        # Verifica as ocorrências da letra na palavra
         posicao = 0
         for letra in palavra_secreta:
             if chute.lower() == letra.lower():
                 palavra_descoberta[posicao] = letra
             posicao += 1
+        print("Uhuuuuu a letra {} foi encontrada!".format(letra))
     else:
+        # Caso a letra não exista atualiza a qtd de erros
         qtd_erros += 1
+        print("Xiiiii a letra {} não existe na palavra secreta!".format(letra))
+        print("Você errou {} vezes de {} possíveis!".format(qtd_erros, qtd_tentativas))
 
-    print(palavra_descoberta)
+    # Mostra a palavra descoberta até o momento
+    print("\n {}".format(palavra_descoberta))
+    print("\n")
+    # Verifica se qtd de erros possíveis foi ultrapassada
     enforcou = qtd_erros == qtd_tentativas
+    # Verifica se a palavra secreta foi descoberta
     ganhou =  '_' not in palavra_descoberta
 
 if ganhou:
-    print("Parabéns, você acertou a palavra secreta!")
+    print("              \o/")
+    print("\n")
+    print("\nParabéns, você acertou a palavra secreta!\n")
 else:
-    print("Você se enforcou danada!")
+    print("\nVocê se enforcou!\n")
